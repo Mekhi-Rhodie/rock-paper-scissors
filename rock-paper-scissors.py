3a705a0293ce5a318e174e23e1dd0673043f7545
Mekhi Rhodie <mekhirhodie@gmail.com>
2025-02-06 14:47:57 -0500

    Update rock-paper-scissors.py
    - add function to determine the winner of the round

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,2 +1,14 @@
 import random
 import os 
+
+choices = ["Rock", "Paper", "Scissors"]
+
+def determine_winner(player, ai):
+    if player == ai:
+        return "Tie"
+    elif (player == "Rock" and ai == "Scissors") or \
+         (player == "Scissors" and ai == "Paper") or \
+         (player == "Paper" and ai == "Rock"):
+        return "Win"
+    else:
+        return "Lose"
